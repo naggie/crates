9211a1b0d6383f66254d8ea1e730aced51acae57
Callan Bryant <callan.bryant@gmail.com>
2015-05-25 20:48:18 +0100

    quick crawl command

--- index/management/commands/crawl.py
+++ index/management/commands/crawl.py
@@ -4,11 +4,25 @@ Crawls a directory for music files and adds to the database.
 Optionally removes files after adding them to crates.
 '''
 
-from os import unlink
-from os.path import abspath
-
 from django.conf import settings
 from django.core.management.base import BaseCommand, CommandError
 
+from index.crawler import FileCrawler
+
 class Command(BaseCommand):
-    pass
+
+    missing_args_message = "Specify <directory> to crawl"
+
+    def add_arguments(self, parser):
+        parser.add_argument('directories', nargs='+', type=str)
+
+    def handle(self, *args, **options):
+        crawler = FileCrawler()
+
+        # TODO external iterator for crawler via generator.
+        # TODO ...progress indication here
+        for directory in options['directories']:
+            crawler.crawl(directory)
+
+
+
