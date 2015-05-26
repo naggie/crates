09a62754c694f215479a33de6362393c249d9169
Callan Bryant <callan.bryant@gmail.com>
2015-05-26 19:47:55 +0100

    streaming CAS server for improved memory usage in dev

--- index/views.py
+++ index/views.py
@@ -1,17 +1,36 @@
+from os import stat
 from django.shortcuts import render
-
 from django.http import HttpResponse
 from django.views.generic import View
-
+from django.http import FileResponse
 from cas import BasicCAS
 
 cas = BasicCAS()
 
 class Cas(View):
-        def get(self, request, ref):
-            filepath = cas.select(ref)
+    # TODO Subclass this to include actual filepath and better name. Based on audioFile.map
+    def get(self, request, ref):
+        '''
+        Serve a file from the CAS. Not scalable, as it ties up an entire
+        UWSGI worker. However, it's better than simply f.read()ing a file into
+        a HttpResponse which loads the entire file into memory.
 
-            #  grossly inefficient. Replace with streamer for development, and
-            #  X-Sendfile for production.
-            with open(filepath,'rb') as f:
-                return HttpResponse(f.read())
+        Instead, FileResponse (a subclass of StreamingHttpResponse) streams the
+        file in chunks. For a big ubuntu ISO, the memory usage went down from
+        around 1000MB to just 30MB. This was measured using the memory_profiler
+        module.
+
+
+        For (scalable) production usage, an X-Sendfile mechanism is required
+        and will be implemented soon.
+        '''
+        filepath = cas.select(ref)
+
+        response = FileResponse(
+            open(filepath,'rb'),
+            content_type="application/octet-stream"
+        )
+
+        response['Content-Length'] = stat(filepath).st_size
+        return response
+
