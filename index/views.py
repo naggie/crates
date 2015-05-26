b0ad34d7bf39f090ce64fe8295cf60c3cc9ee939
Callan Bryant <callan.bryant@gmail.com>
2015-05-26 20:38:47 +0100

    queryable database dump

--- index/views.py
+++ index/views.py
@@ -2,8 +2,14 @@ from os import stat
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import View
-from django.http import FileResponse
+from django.http import FileResponse,HttpResponse
+from django.core.serializers import get_serializer
 from cas import BasicCAS
+
+from models import AudioFile
+
+# TODO: authentication for Peers via middleware and decorators
+
 
 cas = BasicCAS()
 
@@ -33,4 +39,25 @@ class Cas(View):
 
         response['Content-Length'] = stat(filepath).st_size
         return response
+
+class DumpIndex(View):
+    '''Streaming JSON serialised dump of entire database.'''
+    model = AudioFile
+
+    def get(self,request):
+        # yet another example of the many layers of abstraction that django
+        # enforces for no real benefit. Hey, Java called. They want their
+        # string objects back!
+        serialize = get_serializer('json')().serialize
+        # N.B. request.dict is a http.QueryDict with multiple possible values
+        # for the same key. Change to singular with dict()
+        queryset = self.model.objects.filter(**request.GET.dict())
+
+        # TODO: this really needs to be streaming
+        response = HttpResponse(content_type="application/json")
+        serialize(queryset,stream=response)
+        return response
+
+
+
 
