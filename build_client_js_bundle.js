8e4e99f941c4d6a8d9c716e310e4d81db0719e43
Callan Bryant <callan.bryant@gmail.com>
2015-06-26 22:48:46 +0100

    add transformers+build options/comments

---
+++ build_client_js_bundle.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+// I'm going to trial bundling programatically, as then a global install of
+// browserify is not necessary in addition to local dependencies such as react
+// itself.
+//
+// We might want a watch mode. Maybe the answer is watchify or something gulp
+// powered such as @jimjibone's https://gitlab.com/snippets/5920 . I suspect
+// the latter will be useful if we end up using scss as well.
+//
+// We should switch on NODE_ENV=production|debug which already tells react to
+// remove debug messages or not.
+//
+// Alternatively, browserify supports full configuration in package.json.
+// Perhaps we should use that -- I've added the equivalent.
+var browserify = require('browserify')
+var fs = require('fs')
+
+
+var b = browserify()
+b.add('./js/index.js')
+
+if (process.env.NODE_ENV == 'production') {
+    b.transform({
+        global: true,
+    },'uglifyify')
+}
+
+b.transform({es6:true},'reactify')
+
+var dest = fs.createWriteStream('./static/bundle.js')
+
+b.bundle().pipe(dest)
