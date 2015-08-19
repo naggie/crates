9e18e8ccc58dcdd2def7170ccf80d283cfd99e9f
Callan Bryant <callan.bryant@gmail.com>
2015-08-19 22:40:51 +0100

    center white for short cover art

--- index/util.py
+++ index/util.py
@@ -3,6 +3,7 @@ from cas.models import cas
 from django.conf import settings
 from io import BytesIO
 
+
 def make_thumbnail_ref(data):
     """
     Makes a thumbnail that is 256x256 (default) fitting the width, truncating
@@ -12,37 +13,47 @@ def make_thumbnail_ref(data):
     prevent making the image worse, especially as the responsive web interface
     already scales the image.
     """
-    original = BytesIO(data)
+    im = Image.open(BytesIO(data))
 
-    im = Image.open(original)
-
-    if im.size[0] < 256:
-        # NOTE: cropping past the image boundary produces a larger image with
-        # black gaps
-        im = im.crop((
-            0,
-            0,
-            im.size[0],
-            im.size[0],
-        ))
+    if im.size[0] > settings.THUMBNAIL_WIDTH:
+        WIDTH = settings.THUMBNAIL_WIDTH
     else:
-        im.thumbnail((
-            settings.THUMBNAIL_WIDTH,
-            im.size[1]
-        ),resample=Image.LANCZOS)
+        WIDTH = im.size[0]
 
-        im = im.crop((
-            0,
-            0,
-            settings.THUMBNAIL_WIDTH,
-            settings.THUMBNAIL_WIDTH
-        ))
+
+    # scale width
+    im.thumbnail((
+        WIDTH,
+        im.size[1]
+    ),resample=Image.LANCZOS)
+
+    # chop height
+    im = im.crop((
+        0,
+        0,
+        WIDTH,
+        im.size[1],
+    ))
+
+    # find the dominant colour (k-grouping)
+    #...
+
+    # create a substrate to paste on to with the dominant colour
+    substrate = Image.new('RGB',(WIDTH,WIDTH),(255,255,255))
+
+    # paste in middle if short, or truncate bottom if tall
+    if im.size[1] < WIDTH:
+        offset = (WIDTH - im.size[1])/2
+    else:
+        offset = 0
+
+    substrate.paste(im,(0,offset))
 
     # emulate a file again as tobytes doesn't play well with anything other than raw
-    thumbnail = BytesIO()
-    im.save(thumbnail,'jpeg',encoder_name='jpeg',quality=90,optimize=True)
+    outdata = BytesIO()
+    substrate.save(outdata,'jpeg',encoder_name='jpeg',quality=90,optimize=True)
 
-    return cas.insert_blob(thumbnail.getvalue())
+    return cas.insert_blob(outdata.getvalue())
 
 
 
