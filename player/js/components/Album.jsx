68d5383920aa4bad827137b0c3fb8e1267e85814
Callan Bryant <callan.bryant@gmail.com>
2015-08-19 18:51:03 +0100

    more consistent grid usage

--- player/js/components/Album.jsx
+++ player/js/components/Album.jsx
@@ -15,7 +15,7 @@ var Album = React.createClass({
         var plcstyle = {display:this.state.loaded?'none':'block'}
 
         return (
-            <div className="album pure-u-1 pure-u-md-1-3 pure-u-lg-1-6 pure-u-xl-1-7" key={this.props.id}>
+            <div className="album pure-u-1 pure-u-sm-1-2 pure-u-md-1-4 pure-u-lg-1-6 pure-u-xl-1-8" key={this.props.id}>
                 <img
                     className="pure-img-responsive"
                     onLoad={this.swapImg}
