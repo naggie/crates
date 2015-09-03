fb9470c8ba484988d5852d1af0b3ee8feef22473
Callan Bryant <callan.bryant@gmail.com>
2015-09-03 21:59:11 +0100

    rendering for Playlist

--- player/js/index.js
+++ player/js/index.js
@@ -7,14 +7,14 @@ var Player = require('./components/Player.jsx')
 var Playlist = require('./components/Playlist.jsx')
 
 
-var Test = React.createClass({
-    render: function() {
-        return <div><Playlist /><Player /></div>
-    }
-})
-
-
 React.render(
-    <Test />,
+    <div>
+        <Playlist
+            cover_art_ref="7dd2fb3e4ccff0058f5459346f6fef49bee6e5fae71e979febc1198a20d8e55f"
+            name="Whatbruv, what?"
+            artist="DJ BONE (for a reason)"
+        />
+        <Player />
+    </div>,
     document.getElementById('main')
 )
