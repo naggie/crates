041a4aa7e762e21cbceaf1e5bfbeda0bf170a102
Callan Bryant <callan.bryant@gmail.com>
2015-09-04 11:03:30 +0100

    initial rendering for player

--- player/js/components/Player.jsx
+++ player/js/components/Player.jsx
@@ -3,6 +3,13 @@ var classNames = require('classnames')
 var utils = require('../utils')
 
 var Player = React.createClass({
+    getInitialState: function() {
+        return {
+            elapsed: '0:00',
+            elapsed_percent: 30,
+            total:'9:99',
+        }
+    },
     render: function() {
         return (
             <div className="player pure-g">
@@ -11,25 +18,29 @@ var Player = React.createClass({
                     <i className="fa fa-play"></i>
                     <i className="fa fa-forward"></i>
                 </div>
-                <div className="pure-u-lg-1-24 time">1:20</div>
+                <div className="pure-u-lg-1-24 time">{this.state.elapsed}</div>
                 <div className="pure-u-lg-11-24">
                     <div className="progress">
-                        <div className="bar"></div>
-                        <div className="cursor">
+                        <div className="bar" style={{
+                            width:this.state.elapsed_percent+'%',
+                            backgroundColor:this.props.colour,
+                        }}></div>
+                        <div className="cursor" style={{left:this.state.elapsed_percent+'%'}}>
                             <i className="fa fa-circle"></i>
                         </div>
                     </div>
                 </div>
-                <div className="pure-u-lg-1-24 time">5:24</div>
+                <div className="pure-u-lg-1-24 time">{this.state.total}</div>
                 <div className="pure-u-lg-1-24 buttons">
                     <i className="fa fa-heart-o"></i>
                 </div>
                 <div className="pure-u-lg-2-24 cover">
-                    <img className="pure-img-responsive" src="http://localhost:8000/cas/7dd2fb3e4ccff0058f5459346f6fef49bee6e5fae71e979febc1198a20d8e55f.jpg" />
+                    <img className="pure-img-responsive" src={'/cas/'+this.props.cover_art_ref+'.jpg'} />
                 </div>
                 <div className="pure-u-lg-5-24 info">
-                    <div className="title">Fast Jungle Music</div>
-                    <div className="artist">Various Artists</div>
+                    <div className="title">{this.props.title}</div>
+                    <div className="album">{this.props.album}</div>
+                    <div className="artist">{this.props.album_artist}</div>
                 </div>
             </div>
         )
