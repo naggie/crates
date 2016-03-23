716a372ec9e24d1ef3582b9190c2b7a8ec75f97c
Callan Bryant <callan.bryant@gmail.com>
2016-03-23 23:03:33 +0000

    directories for actions/reducers

--- player/js/index.js
+++ player/js/index.js
@@ -12,7 +12,7 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 
-import rootReducer from './reducers'
+import rootReducer from './reducers/rootReducer.js'
 import App from './containers/App'
 
 
