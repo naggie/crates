4fe998dd3cf7b3d83072b77dfe12ba514a7feeab
Callan Bryant <callan.bryant@gmail.com>
2016-04-03 23:08:31 +0100

    start thunk for browser query

--- player/js/actions/browserActions.js
+++ player/js/actions/browserActions.js
@@ -1,9 +1,11 @@
-
+import utils from '../utils'
 
 export function filter_by_letter(letter) {
-    return {
-        type: 'BROWSER_FILTER_BY_LETTER',
-        letter: letter
+    return dispatch => {
+        dispatch({
+            type: 'BROWSER_FILTER_BY_LETTER',
+            letter: letter,
+        })
     }
 }
 
@@ -12,6 +14,14 @@ export function filter_by_text(text) {
         type: 'BROWSER_FILTER_BY_TEXT',
         text: text
     }
+    utils.get('/albums',query).then((albums) => {
+        ...........
+        this.setState({
+            albums: this.state.albums.concat(albums),
+            loading: false,
+            exhausted: !albums.length,
+        })
+    })
 }
 
 export function clear(text) {
